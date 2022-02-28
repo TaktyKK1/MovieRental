0e8b00a72dd9961aa7f214bbefca275e6195a1c6
Michał Pieczka <33464181+Kortisss@users.noreply.github.com>
2022-02-28 13:59:52 +0100

    Baza danych

---
+++ WypozyczalniaFilmow/Models/Kategoria.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WypozyczalniaFilmow.Models
+{
+    public class Kategoria
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Nazwa { get; set; }
+        public string Opis { get; set; }
+        public ICollection<Film> Filmy { get; set; }
+    }
+}
