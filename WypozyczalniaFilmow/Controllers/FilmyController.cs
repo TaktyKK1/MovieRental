236a2340abd4e8042ee04ee1fa54b98f7a26598b
Michal Pieczka <godz1@interia.pl>
2022-03-14 14:16:52 +0100

    aaa

---
+++ WypozyczalniaFilmow/Controllers/FilmyController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WypozyczalniaFilmow.DAL;
+
+namespace WypozyczalniaFilmow.Controllers
+{
+    public class FilmyController : Controller
+    {
+        FilmyContext db;
+
+        public FilmyController(FilmyContext db)
+        {
+            this.db = db;
+        }
+
+
+        public IActionResult Lista(string nazwaKategorii)
+        {
+            var kategoria = db.Kategorie.Include("Filmy").Where(k=>k.Nazwa.ToUpper() == nazwaKategorii).Single();
+            var Filmy = kategoria.Filmy.ToList();
+            return View(Filmy);
+        }
+    }
+}
---
+++ WypozyczalniaFilmow/ViewComponents/MenuViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WypozyczalniaFilmow.DAL;
+namespace WypozyczalniaFilmow.ViewComponents
+{
+    public class MenuViewComponent : ViewComponent
+    {
+        FilmyContext db;
+
+        public MenuViewComponent(FilmyContext db)
+        {
+            this.db = db;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return await Task.FromResult((IViewComponentResult)View("_Menu", db.Kategorie.ToList()));
+        }
+    }
+}
